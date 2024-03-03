2ac2c32bc580c1faa3b6166778ef786c2e7af32a
StunningSamuel <sami.kanawati2003@outlook.com>
2024-03-03 13:33:55 +0200

    HELP MEEEEEEEEEEEEEE

--- BetterMoodleApp/app/bettermoodle/src/main/java/com/example/bettermoodle/MainActivity.java
+++ BetterMoodleApp/app/bettermoodle/src/main/java/com/example/bettermoodle/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.bettermoodle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.Intent;
+import android.util.Base64;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okhttp3.Request;
 
 public class MainActivity extends AppCompatActivity {
+    OkHttpClient client;
+    String getURL = "http://127.0.0.1:5000/schedule";
 
-    EditText userid; EditText password; Button loginbutton;
+    EditText userid;
+    EditText password;
+    Button loginbutton;
+    TextView loginoutput;
+    String USERNAME, PASSWORD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,23 +42,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         userid = findViewById(R.id.userid);
         password = findViewById(R.id.password);
-        loginbutton = findViewById(R.id.loginbutton); }
+        loginbutton = findViewById(R.id.loginbutton);
+        loginoutput = findViewById(R.id.loginoutput);
+        USERNAME = userid.toString();
+        PASSWORD = password.toString();
+        client = new OkHttpClient();
 
-    public void loginbutton(View view)
-    {
-        Intent intent = new Intent(MainActivity.this, OptionPage2.class);
-        startActivity(intent);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                get();
+            }
+        });
     }
+        public void get(){
+            Request req = new Request.Builder().url(getURL).build();
+            client.newCall(req).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    e.printStackTrace();
+                }
 
-
-        //this function goes to next page and outputs username and password to Logcat(for testing purposes)
-        //loginbutton.setOnClickListener(new View.OnClickListener() {
-            //@Override
-            //public void onClick(View view) {
-                //Intent intent = new Intent(MainActivity.this, OptionPage2.class);
-                //startActivity(intent);
-
-       // });
-
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //                                loginoutput.setText(response.body().string());
+                            Log.d("ommak", "hello");
+                        }
+                    });
+                }
+            });
+        }
 
     }
--- BetterMoodleApp/app/bettermoodle/src/main/java/com/example/bettermoodle/OptionPage2.java
+++ BetterMoodleApp/app/bettermoodle/src/main/java/com/example/bettermoodle/OptionPage2.java
@@ -3,12 +3,20 @@ package com.example.bettermoodle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OptionPage2 extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.optionpage);
+        btn = findViewById(R.id.backbtn);
     }
+    public void Button(View view){
+        setContentView(R.layout.activity_main);
+    }
+
 }
