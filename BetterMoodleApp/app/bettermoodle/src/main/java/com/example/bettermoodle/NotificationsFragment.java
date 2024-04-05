80d4404bb3d6dfe4aae67fede6ba40f5161efb5f
StunningSamuel <sami.kanawati2003@outlook.com>
2024-04-05 15:19:15 +0300

    Notifications Menu MK2
    Took all what was written in the notifications fragment and put it in its own activity and made the fragment take you to the activity via intent.

--- BetterMoodleApp/app/bettermoodle/src/main/java/com/example/bettermoodle/NotificationsFragment.java
+++ BetterMoodleApp/app/bettermoodle/src/main/java/com/example/bettermoodle/NotificationsFragment.java
@@ -1,5 +1,6 @@
 package com.example.bettermoodle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,42 +14,12 @@ import android.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 public class NotificationsFragment extends Fragment {
-
-    ListView notifications;
-    ArrayAdapter<String> arrayAdapter;
-    String[] notificationslist = {"Notification 1", "Notification 2", "Notification 3", "Notification 4"};
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        notifications = findViewById(R.id.announcementplaceholder);
-        arrayAdapter = new ArrayAdapter<String>(this, R.layout.notfi_list, notificationslist);
-        notifications.setAdapter(arrayAdapter);
         // Inflate the layout for this fragment
+        Intent intent = new Intent(getActivity(), NotificationActivity.class);
+        startActivity(intent);
         return inflater.inflate(R.layout.fragment_notifications, container, false);
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.notifcations_menu);
-        MenuItem menuItem = menu.findItem(R.id.Search);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Search for announcement:");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                arrayAdapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
     }
 }
